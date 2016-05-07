d9f3e0d2f197d3962f0eb2734b1e6da518cb2c04
Raahil Shah <rds46@cam.ac.uk>
2016-05-07 08:09:59 +0100

    Added nc module.

---
+++ Code/ns-allinone-3.25/ns-3.25/build/ns3/network-coding.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef NETWORK_CODING_H
+#define NETWORK_CODING_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* NETWORK_CODING_H */
+
